2978f3c62c42a90b18402001d9a814aa0d0f203d
Yurii <135170315+YuriiZiatyk@users.noreply.github.com>
2023-07-02 22:21:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,4 +54,43 @@ bot.hears('🔍 Scan Barcode', (ctx) => {
 bot.hears('📁 Source code', (ctx) => {
   ctx.reply(
     'You can see code of this bot on GitHub. Thanks for stars!',
-    { reply_markup: { inline_keyboard
+    { reply_markup: { inline_keyboard: [[{text: '🔗 GitHub', url: 'https://github.com/Khuzha/oneqrbot'}]] } }
+  )
+})
+
+scanBarcode.enter((ctx) => {
+  ctx.reply(
+    'I`m ready. Send a picture!', 
+    { reply_markup: { keyboard: [['⬅️ Back']], resize_keyboard: true } }
+  )
+})
+
+bot.command('share', async (ctx) => {
+  // Get the user's chat ID
+  const chatId = ctx.message.chat.id;
+
+  // Get the user's most recent scanned QR code or barcode from the database
+  const recentScan = await db.collection('scans').findOne({ chatId });
+
+  // Check if the user has a recent scan to share
+  if (recentScan) {
+    // Ask the user to enter the chat ID of the user they want to share the scan with
+    ctx.reply('Please enter the chat ID of the user you want to share your scan with:');
+
+    // Listen for the user's response
+    bot.on('text', async (ctx) => {
+      // Get the target chat ID from the user's message
+      const targetChatId = ctx.message.text;
+
+      // Share the scan with the target user
+      await bot.telegram.sendMessage(targetChatId, `Scan shared by ${chatId}: ${recentScan.data}`);
+
+      // Let the user know that the scan was shared successfully
+      ctx.reply('Scan shared successfully!');
+    });
+  } else {
+    // Let the user know that they don't have a recent scan to share
+    ctx.reply('You don't have a recent scan to share.');
+  }
+});
+
